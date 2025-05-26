e8de53d423f2cfecab5f7beba79f54a774640c17
Daxu223 <samu.kollin@gmail.com>
2025-05-26 19:31:36 +0300

    Changed project structure. Full-stacking was not full-stacking.

---
+++ osa3/puhelinluettelo/frontend/src/services/persons.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+const baseUrl = "http://localhost:3001/api/persons"
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    // Response data is the POST requests body itself, 
+    // like JSON object { name: example, number: 123 }
+    return request.then(response => response.data)
+}
+
+const deletePerson = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+const update = (newObject, id) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+export default { getAll, create, update, deletePerson }
